f51a5b19c01e702aff72efa3c7a89442921dc2b8
Ben Cleverly <ben.cleverly@outlook.com>
2019-09-24 21:58:01 +0100

    spacing issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container mx-auto h-full flex justify-center items-center">
-        <div>
+        <div class="mx-5 lg:mx-0">
             <h1 class="text-orange-500 font-bold text-5xl mb-4">About me</h1>
             <p class="mb-4">Hello, I'm a Senior Full Stack developer based in the Cotswolds. I work with
                 PHP(Laravel)/MySQL on the back end and on the front end I work with VueJs/GSAP/Bootstrap/TailwindCss and the wonderful
