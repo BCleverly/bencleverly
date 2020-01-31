5b1bd0814d46ba13d982615def65f0cc8bbdb349
Ben Cleverly <ben.cleverly@havashelia.com>
2020-01-31 14:39:13 +0000

    remove old link

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,15 +3,6 @@
 ])
 
 @section('content')
-    <div class="container mx-auto">
-        @if(auth()->check())
-            <div class="flex pt-5">
-                <p class="text-lg">
-                    <a href="{{ route('work.create') }}" class="text-orange-500 hover:text-green-500">Add a new piece or work</a>
-                </p>
-            </div>
-        @endif
-    </div>
     <div class="container mx-auto mb-5 mt-5">
         @if($posts->isEmpty())
             <div class="container mx-auto">
