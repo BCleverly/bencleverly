f41d763a743a47351dd6b2b5b27b6812ef73d270
Ben Cleverly <ben.cleverly@outlook.com>
2020-02-01 18:47:07 +0000

    update wording

--- resources/views/work/index.blade.php
+++ resources/views/work/index.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
     <div class="container mx-auto mb-5 mt-5">
+        @if($works->isEmpty())
+            <div class="container mx-auto">
+                <h1 class="text-4xl text-orange-500 font-bold">No work yet...</h1>
+            </div>
+        @endif
         @foreach($works as $work)
             @include('work.item', ['work' => $work])
         @endforeach
